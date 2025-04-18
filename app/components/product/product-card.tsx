41db6944fa76946a297eae3c405670947776917d
mattiamarcon <mattiamarcon05@gmail.com>
2025-04-18 15:11:33 +0200

    try change types

--- app/components/product/product-card.tsx
+++ app/components/product/product-card.tsx
@@ -5,27 +5,22 @@ import Link from "next/link"
 import { Product, Immagine } from "@/utils/types/types"
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import { supabaseClient } from "@/utils/supabase/client"
+import { useState } from "react"
 
 
 interface ProductCardProps {
-  product: {
-    id: number
-    nome: string
-    descrizione: string
-    prezzo: number
-    categoria: string
-    immagine:string
-  }
+  product: Product
 }
 
 export default function ProductCard({product}:ProductCardProps) {
+
   return (
     <div className="flex justify-center">
       <Card className="w-full max-w-sm overflow-hidden transition-all hover:shadow-lg hover:scale-[1.02] cursor-pointer group">
         <div className="relative w-full pt-[100%] overflow-hidden rounded-t-lg">
           <Link href={`/prodotti/${product.id}`}>
             <Image
-              src={product.immagine}
+              src={product.immagine as unknown as string}
               alt="Immagine prodotto"
               fill
               className="object-cover transition-transform group-hover:scale-105"
@@ -41,25 +36,5 @@ export default function ProductCard({product}:ProductCardProps) {
   )
 }
 
-/*
- <div className="flex justify-center p-6">
-    <Card className="w-full max-w-sm overflow-hidden transition-all hover:shadow-lg hover:scale-[1.02] cursor-pointer group">
-      <div className="relative w-full pt-[100%] overflow-hidden rounded-t-lg">
-        <Image
-          src={product.image}
-          alt="Immagine prodotto"
-          fill
-          className="object-cover transition-transform group-hover:scale-105 absolute"
-        />
-      </div>
-      <CardContent className="">
-        <h3 className="text-xl font-semibold">Jeans slim fit</h3>
-        <div className="pt-2">
-          <span className="text-2xl font-bold">€59,99</span>
-        </div>
-      </CardContent>
-    </Card>
-  </div>
 
-*/
 
